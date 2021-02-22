10030b32f64f9db461971271f9428aa12ef2242b
Viropexis <59497210+Viropexis@users.noreply.github.com>
2021-02-21 20:02:32 -0600

    Update Project_05.cpp
    haha cant use the word system in comments or the damn machine freaks out

--- project05/Project_05.cpp
+++ project05/Project_05.cpp
@@ -7,7 +7,7 @@ using namespace std;
 
 int main(int argc, char * argv[])
 {
-    // Check if proper amount of system arguments
+    // Check if proper amount of arguments
     if (argc != 3) {
         cout << "\n********* Command Line Argument Error *********" << endl;
         cout << "==> Incorrect number of Command Line Arguments!" << endl;
